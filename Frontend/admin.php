8b94de959076e025076695dfa2bbcd5fac5ee18c
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 07:51:08 +0800

    New Added Codes

--- Frontend/admin.php
+++ Frontend/admin.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['id']) || $_SESSION['user_type'] != 'admin') {
+    header("Location: ../Frontend/login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
