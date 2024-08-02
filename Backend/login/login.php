905a6c6bab274f2008b2cea16ce5cdc548535991
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 08:02:24 +0800

    Modified Code

--- Backend/login/login.php
+++ Backend/login/login.php
@@ -4,29 +4,30 @@ include("../db/function.php");
 // die($_SESSION['id']);
 if(isset($_POST['submit'])){
     include("../db/db_connect.php");
-    
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM tbl_users WHERE `username` = '$username' AND `password` = '$password'";
+
+    $username = $con->real_escape_string($_POST['username']);
+    $password = $con->real_escape_string($_POST['password']);
+    $sql = "SELECT * FROM tbl_users WHERE username = '$username' AND password = '$password'";
     $query = $con->query($sql);
-    $row = $query->fetch_array();
+    $row = $query->fetch_assoc();
 
     if($query->num_rows != 0){
         $_SESSION['id'] = $row['id'];
+        $_SESSION['user_type'] = $row['user_type']; // Adding user type to the session
+
         if($row['user_type'] == "farmer"){
-        @header("Location: ../../Frontend/farmers.php");
-        }else if($row['user_type'] == "buyer") {
-        @header("Location: ../../Frontend/buyers.php");
-        }else if($row['user_type'] == "admin"){
-        @header("Location: ../../Frontend/admin.php");
+            header("Location: ../../Frontend/farmers.php");
+        } else if($row['user_type'] == "buyer") {
+            header("Location: ../../Frontend/buyers.php");
+        } else if($row['user_type'] == "admin"){
+            header("Location: ../../Frontend/admin.php");
         }
         exit();
     } else {
-      // die("Error: ".$con->error);
+        // die("Error: ".$con->error);
         $_SESSION['status'] = "Incorrect Username or Password";
-        header('location: ../../Frontend/login.php');
+        header('Location: ../../Frontend/login.php');
         exit();
-        
     }
 }
 ?>
