e4351a8c1d9634972c6998fd230d372d9f68b824
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 09:07:56 +0800

    New Added

---
+++ sidebar/sidebar-buyer.php
@@ -0,0 +1,50 @@
+<div class="sidebar">
+            <div class="profile-section">
+                <div class="profile-image"></div>
+                <div class="profile-name">Farmers FullName</div>
+            </div>
+            <nav>
+                <ul>
+                    <li><button class="nav-button" style="background-color: #52B788;"> <i class="fas fa-globe"></i>  Browse Livestock</button></li>
+                    <li><button class="nav-button"><i class="fas fa-gavel"></i> Auction Livestock</button></li>
+                    <li><button class="nav-button"><i class="fas fa-inbox"></i> Inbox</button></li>
+                    <li><button class="nav-button"><i class="fas fa-history"></i> Buy History</button></li>
+                    <li><button class="nav-button"><i class="fas fa-comments"></i> Open Forum</button></li>
+                </ul>
+            </nav>
+            
+            <div class="filters">
+                <h3>SEARCH FILTER</h3>
+                <h5>By Category</h5>
+                <div class="filter-categories">
+                    <label>
+                        <input type="checkbox" name="category" value="cows">
+                        Cows
+                    </label>
+                    <label>
+                        <input type="checkbox" name="category" value="pigs">
+                        Pigs
+                    </label>
+                    <label>
+                        <input type="checkbox" name="category" value="chickens">
+                        Chickens
+                    </label>
+                    <label>
+                        <input type="checkbox" name="category" value="goats">
+                        Goats
+                    </label>
+                    <label>
+                        <input type="checkbox" name="category" value="turkey">
+                        Turkey
+                    </label>
+                    <button class="confirm-button">FILTER</button>
+                </div>
+            </div>
+
+            <div class="logout">
+                <a href="../../Backend/login/logout.php">
+                    <button class="logout-button">LOG OUT</button>
+                </a>
+            </div>
+
+        </div>
