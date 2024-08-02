5e97648e9ea34b520660c84adcc6e25585cf4a43
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 11:15:59 +0800

    New Added

--- Frontend/Buyer/openforum.php
+++ Frontend/Buyer/openforum.php
@@ -11,8 +11,9 @@ if (!isset($_SESSION['id']) || $_SESSION['user_type'] != 'buyer') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LivestoX - Auction Page</title>
-    <link rel="stylesheet" href="../../css/farmers.css">
+    <title>LivestoX - Open Forum Page</title>
+    <link rel="stylesheet" href="../../css/dashboard.css">
+    <link rel="stylesheet" href="../../css/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
