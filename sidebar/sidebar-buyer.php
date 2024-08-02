7e67772d9655d18e2a9bbfb6e548791a4229c7ac
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 10:30:43 +0800

    New Added

--- sidebar/sidebar-buyer.php
+++ sidebar/sidebar-buyer.php
@@ -5,11 +5,31 @@
             </div>
             <nav>
                 <ul>
-                    <li><button class="nav-button" style="background-color: #52B788;"> <i class="fas fa-globe"></i>  Browse Livestock</button></li>
-                    <li><button class="nav-button"><i class="fas fa-gavel"></i> Auction Livestock</button></li>
-                    <li><button class="nav-button"><i class="fas fa-inbox"></i> Inbox</button></li>
-                    <li><button class="nav-button"><i class="fas fa-history"></i> Buy History</button></li>
-                    <li><button class="nav-button"><i class="fas fa-comments"></i> Open Forum</button></li>
+                    <li>
+                        <a href="dashboard.php" class="nav-button-link">
+                            <button class="nav-button" style="background-color: #52B788;"> <i class="fas fa-globe"></i>  Browse Livestock</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="auction.php" class="nav-button-link">
+                            <button class="nav-button"><i class="fas fa-gavel"></i> Auction Livestock</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="inbox.php" class="nav-button-link">
+                            <button class="nav-button"><i class="fas fa-inbox"></i> Inbox</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="buyhistory.php" class="nav-button-link">
+                            <button class="nav-button"><i class="fas fa-history"></i> Buy History</button>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="openforum.php" class="nav-button-link">
+                            <button class="nav-button"><i class="fas fa-comments"></i> Open Forum</button>
+                        </a>
+                    </li>
                 </ul>
             </nav>
             
