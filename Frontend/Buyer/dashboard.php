616df117130557de8e96864528e9532785ad8d23
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-02 08:46:05 +0800

    Change FileName

--- Frontend/Buyer/dashboard.php
+++ Frontend/Buyer/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (!isset($_SESSION['id']) || $_SESSION['user_type'] != 'admin') {
-    header("Location: ../Frontend/login.php");
+if (!isset($_SESSION['id']) || $_SESSION['user_type'] != 'buyer') {
+    header("Location: ../../Frontend/login.php");
     exit();
 }
 ?>
@@ -12,7 +12,7 @@ if (!isset($_SESSION['id']) || $_SESSION['user_type'] != 'admin') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LivestoX</title>
-    <link rel="stylesheet" href="../css/farmers.css">
+    <link rel="stylesheet" href="../../css/farmers.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
