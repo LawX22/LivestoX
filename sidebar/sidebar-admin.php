41feb01cdaa58972af161feff1261767ebad217a
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-04 20:25:24 +0800

    New Added

--- sidebar/sidebar-admin.php
+++ sidebar/sidebar-admin.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
             <div class="profile-section">
                 <div class="profile-image"></div>
-                <div class="profile-name">Admin FullName</div>
+                <div class="profile-name"><?php echo htmlspecialchars($first_name . ' ' . $last_name); ?></div>
             </div>
             <nav>
                 <ul>
