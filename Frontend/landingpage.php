97c093b11418dc0d0290c7c49545fb945e6b2aaa
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-08-05 08:12:59 +0800

    New Added

---
+++ Frontend/landingpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LivestoX - Online Marketplace</title>
+    <link rel="stylesheet" href="../css/landingpage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <div class="logo-circle">Logo</div>
+            <h1>LivestoX</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="intro">
+            <h2><span>Online</span> Marketplace</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <a href="login.php" class="get-started-link"><button>Get Started</button></a>
+        </section>
+        <section class="design-details">
+            <div class="details">More Design Details Here</div>
+        </section>
+    </main>
+</body>
+</html>
